888a706dfcd082863dbd293e2fc045aaeac9d3e2
Kim Geunyeol <wpfhorez4@naver.com>
2023-03-20 16:06:41 +0900

    usync 코드 수정(log 삭제 및 파라미터 순서 변경 등)

--- app/uconvenience/src/main/java/com/git/uconvenience/USync.kt
+++ app/uconvenience/src/main/java/com/git/uconvenience/USync.kt
@@ -1,32 +1,23 @@
 package com.git.uconvenience
 
-import android.util.Log
 import java.util.*
 
-class USync(tag: String = "geunyeol") {
-    private val USYNC_TAG: String
-
+class USync {
     private val lock = Object()
     private var loop = true
     private var startLockTime: Long = 0
     private var currentLockTime: Long = 0
 
-    init {
-        USYNC_TAG = tag
-    }
-
     interface USyncInterface {
         fun syncFail()
     }
 
-    fun nowLock(waitMillis: Long = 3, timeoutMillis: Long = 3000, tag: String = USYNC_TAG, syncFail: USyncInterface? = null) {
+    fun nowLock(syncFail: USyncInterface? = null, waitMillis: Long = 3, timeoutMillis: Long = 3000) {
         synchronized(lock) {
             startLockTime = Date().time
             currentLockTime = startLockTime
-            Log.v(tag, "Synchronized lock start")
             while(loop) {
                 if(currentLockTime - startLockTime > timeoutMillis) {
-                    Log.e(USYNC_TAG, "Synchronized lock timeout")
                     syncFail?.syncFail()
                     break
                 } else {
@@ -35,25 +26,20 @@ class USync(tag: String = "geunyeol") {
                 lock.wait(waitMillis)
             }
             loop = true
-            Log.d(tag, "Synchronized lock end")
         }
     }
 
-    fun nowNotifyAll(tag: String = USYNC_TAG) {
+    fun nowNotifyAll() {
         synchronized(lock) {
-            Log.v(tag, "Synchronized notifyAll start")
             loop = false
             lock.notifyAll()
-            Log.d(tag, "Synchronized notifyAll end")
         }
     }
 
-    fun nowNotify(tag: String = USYNC_TAG) {
+    fun nowNotify() {
         synchronized(lock) {
-            Log.v(tag, "Synchronized notify start")
             loop = false
             lock.notify()
-            Log.d(tag, "Synchronized notify end")
         }
     }
 }
